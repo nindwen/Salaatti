178e04b6c582360eac91da784ca89db8e951db94
nindwen <me@nindwen.blue>
2016-02-16 21:28:05 +0200

    Make preview smarter

--- models/preview.js
+++ models/preview.js
@@ -1,37 +1,36 @@
 var fs = require("fs");
 var hl = require("highlight.js");
+var readChunk = require('read-chunk'); // npm install read-chunk 
+var fileType = require('file-type');
 function getExtension(filename) {
       return filename.substr((~-filename.lastIndexOf(".") >>> 0) + 2);
+}
+
+function getType(type) {
+      return type.mime.replace(/\/.*$/,'');
 }
 
 module.exports = {
       parse: function(req,res) {
             var fname = req.params[0];
-            var extension = getExtension(fname);
+            var buffer = readChunk.sync('/home/public/files/' + fname, 0, 262);
+            var type = getType(fileType(buffer));
+            console.log(type);
 
-            switch(extension) {
-                  case "png":
-                  case "jpg":
-                  case "jpeg":
-                  case "bmp":
-                        res.render("preview",{ file: fname, type: "image" });
+            switch(type) {
+                  case "image":
+                        res.render("preview",{ file: fname, type: type });
                         break;
-                  case "ogg":
-                  case "midi":
-                  case "mid":
-                  case "mp3":
-                  case "wav":
-                        res.render("preview",{ file: fname, type: "music" });
+                  case "audio":
+                        res.render("preview",{ file: fname, type: type });
                         break;
-                  default:
+                  case "text":
                         var data = fs.readFileSync("/home/public/files/" + fname,"utf8");
                         var hilit = hl.highlightAuto(data);
-                        if(hilit.language == "") {
-                              res.send("lol ei sry");
-                        }
-                        else {
-                              res.render("preview",{ file: fname, type: "text", data: hilit.value });
-                        }
+                        res.render("preview",{ file: fname, type: type, data: hilit.value });
+                        break;
+                  default:
+                        res.render("preview",{ file: fname, type: "other"});
                         break;
             }
       }
